72b2333e0df5f7984ba97fe7b56373b33e5ddabc
DevPeter <polalekan526@gmail.com>
2023-08-15 20:10:00 +0100

    form added to login page

--- src/Login.js
+++ src/Login.js
@@ -3,7 +3,7 @@ import "./style.css"
 const Login = () =>{
     return(
         <div>
-         <nav className="flow-root flex group  w-full columns-2 border-b-2 px-4 py-6 my-1">
+         <nav className="flow-root flex group mb-10 w-full columns-2  px-4 py-6 my-1">
        
        <div className="float-left flex">
 
@@ -17,12 +17,37 @@ const Login = () =>{
         
          <li className="  px-2 py-2 mt-3 mr-4 rounded-lg">Don't have an account?</li>
     
-        <li className="  border-2 px-3 py-2 mt-3 rounded-lg">Sign Up</li>
+        <li className="  border-2 px-3 py-2 mt-3 rounded-lg bg-blue-600 text-white shadow-blue-300 shadow-xl">Sign Up</li>
          </div>
          
  
        </ul>
      </nav>
+
+     <div className=" flex justify-center items-center">
+      <div className="bg-white p-16 w-44 rounded shadow-md loginForm ">
+        <h2 className="text-3xl font-bold mb-6 text-center">Welcome Back!</h2>
+        <form>
+          <div className="mb-4">
+          <label className="my-10 text-2xl">Email</label>
+            <div className="flex items-center border rounded px-3 mt-4 py-2">
+              <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M10 12H2m8 0v6m0-6l6-6-6 6z"></path></svg>
+              <input type="text" className="w-full outline-none" placeholder="Email" />
+            </div>
+          </div>
+          <div className="mb-6">
+          <label className="my-10 text-2xl">Password</label>
+            <div className="flex items-center border rounded px-3 mt-4 py-2">
+              <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 14l9-5-9-5-9 5 9 5z"></path></svg>
+              <input type="password" className="w-full outline-none" placeholder="Password" />
+            </div>
+          </div>
+          <button className="bg-blue-600 hover:bg-blue-600 shadow-blue-500 shadow-2xl text-white font-semibold py-4 px-4 rounded w-full">Log in</button>
+
+        </form>
+      </div>
+    </div>
+
      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="svg" preserveAspectRatio="none" style={{ position: "absolute", top:320, left: 0, width: "100%", height: "60%" }}>
   <defs>
     <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
