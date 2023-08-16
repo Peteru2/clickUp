08677b138830b0859da402633b64c6ad96068638
DevPeter <polalekan526@gmail.com>
2023-08-16 01:08:14 +0100

    Login page completed

--- src/Login.js
+++ src/Login.js
@@ -1,66 +1,77 @@
-import img from "./clickIcon.jpg"
+import img from "./clickIcon.png"
 import "./style.css"
 const Login = () =>{
     return(
-        <div>
-         <nav className="flow-root flex group mb-10 w-full columns-2  px-4 py-6 my-1">
+        <div className="bg-gray-100 logBody">
+         <nav className="flow-root flex group mb-6 md:mb-10  w-full columns-2  px-7 py-6 my-1">
        
        <div className="float-left flex">
 
         <img src={img} className=" w-8 ml-0" alt="Home" />
-        <h2 className="font-bold text-3xl">Click Up  </h2>
+        <h2 className="font-bold ml-1 text-3xl">Click Up  </h2>
         
        </div>
       
        <ul className="float-right flex">
          <div  className="flex">          
         
-         <li className="  px-2 py-2 mt-3 mr-4 rounded-lg">Don't have an account?</li>
+         <li className=" hidden px-2 py-2 mt-3 mr-4 rounded-lg">Don't have an account?</li>
     
-        <li className="  border-2 px-3 py-2 mt-3 rounded-lg bg-blue-600 text-white shadow-blue-300 shadow-xl">Sign Up</li>
+        <li className="   px-3 py-2 mt-3 rounded-lg bg-blue-600 text-white shadow-blue-300 shadow-xl">Sign Up</li>
          </div>
          
  
        </ul>
      </nav>
 
-     <div className=" flex justify-center items-center">
-      <div className="bg-white p-16 w-44 rounded shadow-md loginForm ">
+     <div className=" flex justify-center items-center ">
+      <div className="bg-white p-10 md:p-16  w-44 rounded-lg shadow-md loginForm ">
         <h2 className="text-3xl font-bold mb-6 text-center">Welcome Back!</h2>
         <form>
           <div className="mb-4">
-          <label className="my-10 text-2xl">Email</label>
+          <label className="my-5 ">Email</label>
             <div className="flex items-center border rounded px-3 mt-4 py-2">
-              <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M10 12H2m8 0v6m0-6l6-6-6 6z"></path></svg>
+            <i className="fa fa-envelope w-5 h-5 text-gray-400 mr-2 mt-1"></i>
               <input type="text" className="w-full outline-none" placeholder="Email" />
             </div>
           </div>
           <div className="mb-6">
-          <label className="my-10 text-2xl">Password</label>
+          <label className="my-5">Password   </label>
             <div className="flex items-center border rounded px-3 mt-4 py-2">
-              <svg className="w-5 h-5 text-gray-400 mr-2" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24"><path d="M12 14l9-5-9-5-9 5 9 5z"></path></svg>
+            <i className="fa fa-lock w-5 h-5 text-gray-400 mr-2 mt-1"></i>
               <input type="password" className="w-full outline-none" placeholder="Password" />
             </div>
           </div>
           <button className="bg-blue-600 hover:bg-blue-600 shadow-blue-500 shadow-2xl text-white font-semibold py-4 px-4 rounded w-full">Log in</button>
 
         </form>
+         
       </div>
+    
     </div>
+    <div className="asideBox text-center font-bold">
+      <p className="border-2 w-4  rounded-md">?</p>
+      <p>Help</p>
+    </div>
+    <div className="logDetail">
+    <p className="mt-8 text-white">Don't have an account? sign up</p>
 
-     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" className="svg" preserveAspectRatio="none" style={{ position: "absolute", top:320, left: 0, width: "100%", height: "60%" }}>
+   
+    <p className="mt-2 md:mt-20 px-16  md:p-0 text-white logfooterP">This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.</p>
+    </div>
+     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100"  preserveAspectRatio="none" style={{ position: "absolute", top:390, left: 0, width: "100%", height: "50%" }}>
   <defs>
     <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" style={{ stopColor: "rgb(255,255,0)", stopOpacity: 1 }} />
-      <stop offset="100%" style={{ stopColor: "rgb(255,0,0)", stopOpacity: 1 }} />
+      <stop offset="0%" style={{ stopColor: "rgb(37 99 235)", stopOpacity: 1 }} />
+      <stop offset="100%" style={{ stopColor: "rgb(217 70 239)", stopOpacity: 1 }} />
     </linearGradient>
-    <pattern id="dots-pattern" width="4" height="3" patternUnits="userSpaceOnUse" style={{ position: "absolute", top:320, left: 0, width: "100%", height: "60%" }}>
-      <circle cx="1" cy="1" r="0.4" fill="black"  />
+    <pattern id="dots-pattern" width="4" height="3" patternUnits="userSpaceOnUse" style={{ position: "absolute", top:320, left: 0, width: "100%", height: "100%" }}>
+      <circle cx="1" cy="2" r="0.4" fill="rgba(0,0,0,.2)"  />
     </pattern>
   </defs>
-  <path d="M0,28 Q0,10 70,1 H70 Q90,0 100,20 V100 H0 Z" fill="url(#grad1)" />
+  <path d="M0,80 Q0,10 70,1 H70 Q95,0 100,20 V100 H0 Z" fill="url(#grad1)" />
 
-  <rect x="0" y="15" width="100%" height="75%" fill="url(#dots-pattern)" />
+  <rect x="8" y="15" width="100%" height="100%" fill="url(#dots-pattern)" />
 </svg>
         </div>
     )
