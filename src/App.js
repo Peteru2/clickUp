b2469f1d2a253264102e3c4033da93a7f1413046
DevPeter <polalekan526@gmail.com>
2023-08-21 23:56:51 +0100

    working on it all

--- src/App.js
+++ src/App.js
@@ -3,20 +3,20 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 // import List from './List';
-// import Load from './LoadLogin';
-import Login from './Login'
-import SignUp from './SignUp';
-import SSO from './Sso';
+import Load from './LoadLogin';
+// import Login from './Login'
+// import SignUp from './SignUp';
+// import SSO from './Sso';
 function App() {
   return (
    <>
     <Routes>
-     <Route path ="/" element = {<Login />} />
+     {/* <Route path ="/" element = {<Login />} />
        <Route path ="/SignUp" element = {<SignUp />} />
-      <Route path ="/SignUp/Sso" element = {<SSO />} />
+      <Route path ="/SignUp/Sso" element = {<SSO />} /> */}
 
 
-      {/* <Route path ="/" element = {<Load />} /> */}
+      <Route path ="/" element = {<Load />} />
       {/* <Route path ="/About" element = {<About />} /> */}
       
 
--- src/List.js
+++ src/List.js
@@ -25,19 +25,37 @@ const App = () => {
     <NavSec />
   
     
-      <div className="  border-2 rounded-md p-2 mx-4">
+      <div className="  border-2 rounded-md p-2 mx-6">
           <div className="flex justify-between">
+         
           <p className="text-gray-400 text-sm">Team Space <i className="fa fa-chevron-right"></i> Projects <i className="fa fa-chevron-right"></i> Project 1</p>
-          <p className="text-gray-400 text-sm">SHOW CLOSED</p>
+          <p className="text-gray-400 text-sm"><i className = "fa fa-check text-sm mr-1"></i> SHOW CLOSED</p>
           </div>
         <div className="flex mt-2 group">
-            <div className="mr-2 text-black hover:bg-gray-200 hover:rounded hover:p-0.5"><i className="fa fa-chevron-down border-2 text-xs p-0.5 rounded-full border-black"></i> Project 1</div>
+                <div className="mr-2 text-black hover:bg-gray-200 hover:rounded hover:p-0.5">
+                  <span className="fa-stack fa-xs">
+        <i className="fa fa-circle fa-stack-2x"></i>
+    <i className="fa fa-chevron-down fa-stack-1x fa-inverse"></i>
+  </span>
+  Project 1
+    
+  </div>
+  </div>
           
             <div className=" text-sm text-gray-400 mr-4 hover:bg-gray-200 hover:rounded hover:px-1">+ NEW TASK</div>
              <div className="group-hover:flex hidden text-sm text-gray-400 mr-4 hover:bg-gray-200 hover:rounded hover:px-1">ADD DESCRIPTION</div>
            <div className="group-hover:flex hidden text-sm text-gray-400 mr-4 hover:bg-gray-200 hover:rounded hover:px-1">ADD COMMENT</div>
 
         </div>
+        <div className="flex mt-2 group">
+                <div className="mr-2 text-black hover:bg-gray-200 hover:rounded hover:p-0.5">
+                  <span className="fa-stack fa-xs">
+        <i className="fa fa-circle fa-stack-2x"></i>
+    <i className="fa fa-chevron-down fa-stack-1x fa-inverse"></i>
+  </span>
+  Project 1
+  </div>
+  
       </div>
      
      
