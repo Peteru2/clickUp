1a03ff38f3c5b9ee62f72759ec98ac17ec4fecf6
DevPeter <polalekan526@gmail.com>
2023-08-31 12:38:15 +0100

    Video added on landing page

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,18 +1,23 @@
 import Navbar from "./Components/Navbar";
-import img from "./clickIcon.png"
+// import img from "./clickIcon.png"
 import SVG from "./svg";
+import Vid1 from "./vid1.webm"
 
 const Landing = () => {
     return ( 
         <>
             <Navbar />
                 <section className="mx-14 mt-32">
-                    <div className="grid grid-cols-2 mx-10">
+                    <div className="grid grid-cols-2 gap-20 mx-20">
                             <div>
-                                <p className="text-5xl font-bold">One app to replace them all.</p>
+                                <p className="text-7xl font-bold">One app to replace them all.</p>
+                                
                             </div>
                             <div>
-                                <img src={img} alt = "sideImg"  className="mx-20"/>
+                          <video autoPlay loop muted playsInline>
+                            <source src={Vid1} type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
                             </div>
                     </div>
 
